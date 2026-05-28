21c6ae8dd667b2b25ec37e1349e6dd9343036e6a
Uriel <mamaniuriel592@gmail.com>
2026-05-28 12:28:51 -0300

    ,

---
+++ TP4-EJERCICIO-N9/src/tp4/ejercicio/n9/TP4EJERCICION9.java
@@ -0,0 +1,41 @@
+
+package tp4.ejercicio.n9;
+
+import java.util.Scanner;
+
+public class TP4EJERCICION9 {
+
+   
+    public static void main(String[] args) {
+      
+        
+   Scanner sc = new Scanner(System.in);
+
+  
+        boolean[] pagos = new boolean[10];
+
+        int pagaron = 0;
+        int deudores = 0;
+
+        
+  
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Alumno " + (i + 1));
+            System.out.println("Pago la cuota? (true/false): ");
+            pagos[i] = sc.nextBoolean();
+
+            if (pagos[i]) {
+                pagaron++;
+            } else {
+                deudores++;
+            }
+        }
+
+        double porcentaje = (pagaron * 100.0) / 10;
+
+        System.out.println("Cantidad de pagos: " + pagaron);
+        System.out.println("Cantidad de deudores: " + deudores);
+        System.out.println("Porcentaje de cobranza: " + porcentaje + "%");
+     
+  }
+}     
